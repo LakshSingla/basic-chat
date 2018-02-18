7a5e3974e83c4eacfaed03e36d94877e3355e755
Laksh Singla <lakshsingla@gmail.com>
2018-02-18 20:32:45 +0530

    Removed the Vue boilerplate

--- client/static/js/main.js
+++
@@ -1,5 +0,0 @@
-// let heading = document.getElementsByTagName('h1')[0];
-//          heading.style.left = (document.documentElement.clientWidth - heading.getBoundingClientRect().width)/2 + "px";
-//          window.onresize = function(){
-//              heading.style.left = (document.documentElement.clientWidth - heading.getBoundingClientRect().width)/2 + "px";
-//          }
